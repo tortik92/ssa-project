9b0d58fa0c7c5285b3c716637e144663f0922893
tortik92 <kompik110@gmail.com>
2023-09-15 20:01:53 +0200

    [SSAP-22] Server Init

---
+++ server/index.php
@@ -0,0 +1,26 @@
+<?php
+    header('Content-Type: application/json; charset=utf-8');
+
+    $uid = "";
+    if (isset($_GET['uid'])) { // check for them and set them so
+        $uid = $_GET['uid'];
+    }
+
+    $json = file_get_contents("games.json");
+    $json_data = json_decode($json, true);
+
+    if($uid != "") {
+        for($i = 0; $i < sizeof($json_data); $i++) {
+            if($json_data[$i]["uid"] == $uid) {
+                echo json_encode($json_data[$i]);
+            }
+        }
+        echo json_encode(array());
+    }
+    else {
+        $outputJSON = json_encode($json_data);
+        echo $outputJSON;
+    }
+?>
+
+
