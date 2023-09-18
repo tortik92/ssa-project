6b474efb320e4aedda58b8a32e672f42e8e4889e
ayanwhb <200118@studierende.htl-donaustadt.at>
2023-09-18 08:48:23 +0200

    [SSAP-19] Fixed code smell

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -1,251 +1,245 @@
-function checkLoginCredentials() {
-    let idInput = document.getElementById('device-id-input').value;
-    if (idInput === '123') {
-        window.location.href = './pages/game-selection.html';
+function checkLoginCredentials () { // eslint-disable-line
+  const idInput = document.getElementById('device-id-input').value;
+  if (idInput === '123') {
+    window.location.href = './pages/game-selection.html';
+  } else {
+    alert('Invalid Device-ID. Please try again.');
+  }
+}
+
+function createButton (text, clickHandler) {
+  const button = document.createElement('button');
+  button.innerText = text;
+  button.addEventListener('click', clickHandler);
+  return button;
+}
+
+function createInput (preference) {
+  const inputElement = document.createElement('input');
+  inputElement.type = 'text';
+  inputElement.value = preference.default_value;
+  inputElement.placeholder = 'Type in a value!';
+  inputElement.maxLength = preference.max_length;
+  inputElement.minLength = preference.min_length;
+
+  // Add an input event listener to enforce max length and check min length
+  inputElement.addEventListener('input', function () {
+    const inputValue = inputElement.value;
+    if (inputValue.length > preference.max_length) {
+      inputElement.value = inputValue.slice(0, preference.max_length);
+    }
+    if (inputValue.length < preference.min_length) {
+      inputElement.style.borderColor = 'red';
     } else {
-        alert('Invalid Device-ID. Please try again.');
+      inputElement.style.borderColor = ''; // Reset to default
     }
+  });
+
+  return inputElement;
 }
 
-function createButton(text, clickHandler) {
-    const button = document.createElement('button');
-    button.innerText = text;
-    button.addEventListener('click', clickHandler);
-    return button;
+function incrementPreferenceValue (preferenceValue, maxValue) {
+  const currentValue = parseInt(preferenceValue.innerText);
+  if (currentValue < maxValue) {
+    preferenceValue.innerText = currentValue + 1;
+  }
 }
 
-function createInput(preference) {
-    const inputElement = document.createElement('input');
-    inputElement.type = "text";
-    inputElement.value = preference.default_value;
-    inputElement.placeholder = 'Type in a value!';
-    inputElement.maxLength = preference.max_length;
-    inputElement.minLength = preference.min_length;
-
-    // Add an input event listener to enforce max length and check min length
-    inputElement.addEventListener('input', function () {
-        const inputValue = inputElement.value;
-        if (inputValue.length > preference.max_length) {
-            inputElement.value = inputValue.slice(0, preference.max_length);
-        }
-        if (inputValue.length < preference.min_length) {
-            inputElement.style.borderColor = 'red';
-        } else {
-            inputElement.style.borderColor = ''; // Reset to default
-        }
-    });
-
-    return inputElement;
+function decrementPreferenceValue (preferenceValue, minValue) {
+  const currentValue = parseInt(preferenceValue.innerText);
+  if (currentValue > minValue) {
+    preferenceValue.innerText = currentValue - 1;
+  }
 }
 
-function incrementPreferenceValue(preferenceValue, maxValue) {
-    const currentValue = parseInt(preferenceValue.innerText);
-    if (currentValue < maxValue) {
-        preferenceValue.innerText = currentValue + 1;
-    }
+function incrementListPreferenceValue (preferenceValue, list) {
+  const currentValue = preferenceValue.innerText;
+  const currentIndex = list.indexOf(currentValue);
+  const newIndex = (currentIndex + 1) % list.length; // Wrap around to 0 if at the end
+  preferenceValue.innerText = list[newIndex];
 }
 
-function decrementPreferenceValue(preferenceValue, minValue) {
-    const currentValue = parseInt(preferenceValue.innerText);
-    if (currentValue > minValue) {
-        preferenceValue.innerText = currentValue - 1;
-    }
+function decrementListPreferenceValue (preferenceValue, list) {
+  const currentValue = preferenceValue.innerText;
+  const currentIndex = list.indexOf(currentValue);
+  const newIndex = (currentIndex - 1 + list.length) % list.length; // Wrap around to the end if at the beginning
+  preferenceValue.innerText = list[newIndex];
 }
 
-function incrementListPreferenceValue(preferenceValue, list) {
-    const currentValue = preferenceValue.innerText;
-    const currentIndex = list.indexOf(currentValue);
-    const newIndex = (currentIndex + 1) % list.length; // Wrap around to 0 if at the end
-    preferenceValue.innerText = list[newIndex];
+function toggleBooleanPreferenceValue (preferenceValue) {
+  const currentValue = preferenceValue.innerText === 'true'; // Convert to boolean
+  const newValue = !currentValue; // Toggle between true and false
+  preferenceValue.innerText = newValue.toString(); // Convert back to string ('true' or 'false')
 }
 
-function decrementListPreferenceValue(preferenceValue, list) {
-    const currentValue = preferenceValue.innerText;
-    const currentIndex = list.indexOf(currentValue);
-    const newIndex = (currentIndex - 1 + list.length) % list.length; // Wrap around to the end if at the beginning
-    preferenceValue.innerText = list[newIndex];
+function createPreferenceElement (preference) {
+  const preferenceListElement = document.createElement('div');
+  preferenceListElement.classList.add('preference-list-element');
+  const preferenceName = document.createElement('h2');
+  const preferenceNameValuePair = document.createElement('div');
+  preferenceNameValuePair.classList.add('preference-key-value-pair');
+  let preferenceValue = null;
+
+  switch (preference.preference_type) {
+    case 'number':
+      preferenceValue = document.createElement('h2');
+      preferenceValue.innerText = preference.default_value;
+      preferenceName.innerText += preference.preference_name;
+
+      preferenceNameValuePair.append(preferenceName);
+      preferenceNameValuePair.append(preferenceValue);
+      preferenceListElement.append(createButton('-', () => decrementPreferenceValue(preferenceValue, parseInt(preference.min_value))));
+      preferenceListElement.append(preferenceNameValuePair);
+      preferenceListElement.append(createButton('+', () => incrementPreferenceValue(preferenceValue, parseInt(preference.max_value))));
+      break;
+
+    case 'list':
+      preferenceValue = document.createElement('h2');
+      preferenceValue.innerText = preference.list[preference.default_value];
+      preferenceName.innerText += preference.preference_name;
+
+      preferenceNameValuePair.append(preferenceName);
+      preferenceNameValuePair.append(preferenceValue);
+      preferenceListElement.append(createButton('<', () => decrementListPreferenceValue(preferenceValue, preference.list)));
+      preferenceListElement.append(preferenceNameValuePair);
+      preferenceListElement.append(createButton('>', () => incrementListPreferenceValue(preferenceValue, preference.list)));
+      break;
+
+    case 'bool':
+      preferenceValue = document.createElement('h2');
+      preferenceValue.innerText = preference.default_value === true ? 'true' : 'false';
+      preferenceName.innerText += preference.preference_name;
+
+      preferenceNameValuePair.append(preferenceName);
+      preferenceNameValuePair.append(preferenceValue);
+      preferenceListElement.append(createButton('<', () => toggleBooleanPreferenceValue(preferenceValue)));
+      preferenceListElement.append(preferenceNameValuePair);
+      preferenceListElement.append(createButton('>', () => toggleBooleanPreferenceValue(preferenceValue)));
+      break;
+
+    case 'text':
+      preferenceValue = createInput(preference);
+      preferenceName.innerText += preference.preference_name;
+
+      preferenceNameValuePair.append(preferenceName);
+      preferenceNameValuePair.append(preferenceValue);
+
+      preferenceListElement.append(preferenceNameValuePair);
+      break;
+  }
+
+  return preferenceListElement;
 }
 
-function toggleBooleanPreferenceValue(preferenceValue) {
-    const currentValue = preferenceValue.innerText === 'true'; // Convert to boolean
-    const newValue = !currentValue; // Toggle between true and false
-    preferenceValue.innerText = newValue.toString(); // Convert back to string ('true' or 'false')
-}
+function createGameListElement (game) {
+  const listElement = document.createElement('li');
+  listElement.classList.add('list-element');
 
-function createPreferenceElement(preference) {
-    const preferenceListElement = document.createElement('div');
-    preferenceListElement.classList.add('preference-list-element');
-    const preferenceName = document.createElement('h2');
-    const preferenceNameValuePair = document.createElement('div');
-    preferenceNameValuePair.classList.add('preference-key-value-pair');
-    let preferenceValue = null;
+  const gameNameTitle = document.createElement('h2');
+  gameNameTitle.classList.add('game-name');
+  gameNameTitle.innerText = game.name;
 
-    switch (preference.preference_type) {
-        case 'number':
-            preferenceValue = document.createElement('h2');
-            preferenceValue.innerText = preference.default_value;
-            const leftButton = createButton('-', () => decrementPreferenceValue(preferenceValue, parseInt(preference.min_value)));
-            const rightButton = createButton('+', () => incrementPreferenceValue(preferenceValue, parseInt(preference.max_value)));
-            preferenceName.innerText += preference.preference_name;
+  const gameInformationContainer = document.createElement('div');
+  gameInformationContainer.classList.add('game-information-container');
 
-            preferenceNameValuePair.append(preferenceName);
-            preferenceNameValuePair.append(preferenceValue);
-            preferenceListElement.append(leftButton);
-            preferenceListElement.append(preferenceNameValuePair);
-            preferenceListElement.append(rightButton);
-            break;
+  const gameDescription = document.createElement('p');
+  gameDescription.classList.add('game-description');
+  gameDescription.innerText = game.description;
 
-        case 'list':
-            preferenceValue = document.createElement('h2');
-            preferenceValue.innerText = preference.list[preference.default_value];
-            const leftListButton = createButton('<', () => decrementListPreferenceValue(preferenceValue, preference.list));
-            const rightListButton = createButton('>', () => incrementListPreferenceValue(preferenceValue, preference.list));
-            preferenceName.innerText += preference.preference_name;
+  const gameVersion = document.createElement('p');
+  gameVersion.classList.add('game-description');
+  gameVersion.innerText = game.version;
 
-            preferenceNameValuePair.append(preferenceName);
-            preferenceNameValuePair.append(preferenceValue);
-            preferenceListElement.append(leftListButton);
-            preferenceListElement.append(preferenceNameValuePair);
-            preferenceListElement.append(rightListButton);
-            break;
+  gameInformationContainer.append(gameDescription);
+  gameInformationContainer.append(gameVersion);
 
-        case 'bool':
-            preferenceValue = document.createElement('h2');
-            preferenceValue.innerText = preference.default_value === true ? 'true' : 'false';
-            const leftToggleButton = createButton('<', () => toggleBooleanPreferenceValue(preferenceValue));
-            const rightToggleButton = createButton('>', () => toggleBooleanPreferenceValue(preferenceValue));
-            preferenceName.innerText += preference.preference_name;
+  listElement.append(gameNameTitle);
+  listElement.append(gameInformationContainer);
 
-            preferenceNameValuePair.append(preferenceName);
-            preferenceNameValuePair.append(preferenceValue);
-            preferenceListElement.append(leftToggleButton);
-            preferenceListElement.append(preferenceNameValuePair);
-            preferenceListElement.append(rightToggleButton);
-            break;
-
-        case 'text':
-            preferenceValue = createInput(preference);
-            preferenceName.innerText += preference.preference_name;
-
-            preferenceNameValuePair.append(preferenceName);
-            preferenceNameValuePair.append(preferenceValue);
-
-            preferenceListElement.append(preferenceNameValuePair);
-            break;
-    }
-
-    return preferenceListElement;
-}
-
-function createGameListElement(game) {
-    const listElement = document.createElement('li');
-    listElement.classList.add('list-element');
-
-    const gameNameTitle = document.createElement('h2');
-    gameNameTitle.classList.add('game-name');
-    gameNameTitle.innerText = game.name;
-
-    const gameInformationContainer = document.createElement('div');
-    gameInformationContainer.classList.add('game-information-container');
-
-    const gameDescription = document.createElement('p');
-    gameDescription.classList.add('game-description');
-    gameDescription.innerText = game.description;
-
-    const gameVersion = document.createElement('p');
-    gameVersion.classList.add('game-description');
-    gameVersion.innerText = game.version;
-
-    gameInformationContainer.append(gameDescription);
-    gameInformationContainer.append(gameVersion);
-
-    listElement.append(gameNameTitle);
-    listElement.append(gameInformationContainer);
-
-    return listElement;
+  return listElement;
 }
 
 const gameArr = [];
 let selectedGameUID = 0;
 
 addEventListener('DOMContentLoaded', () => {
-    let pathname = window.location.pathname;
-    if (pathname.includes('/ssa-project/website/pages/game-selection.html')) {
-        fetch('https://cakelab.co.nl/ssa-server/')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const gameList = document.getElementById('game-list');
+  const pathname = window.location.pathname;
+  if (pathname.includes('/ssa-project/website/pages/game-selection.html')) {
+    fetch('https://cakelab.co.nl/ssa-server/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const gameList = document.getElementById('game-list');
 
-                data.forEach(game => {
-                    gameArr.push(game);
-                    const listElement = createGameListElement(game);
-                    gameList.append(listElement);
-                })
+        data.forEach(game => {
+          gameArr.push(game);
+          const listElement = createGameListElement(game);
+          gameList.append(listElement);
+        });
 
-                let gameListElements = document.querySelectorAll('.list-element');
+        const gameListElements = document.querySelectorAll('.list-element');
 
-                gameListElements.forEach((listElement, index) => {
-                    listElement.addEventListener('click', () => {
-                        gameListElements.forEach(listElement => {
-                            listElement.classList.remove('selected');
-                        })
-                        listElement.classList.add('selected');
-                        selectedGameUID = gameArr[index].uid;
-                    })
-                })
-            })
-            .catch(err => {
-                console.error('Fetch error:', err);
-            })
-    } else if(pathname.includes('/ssa-project/website/pages/game-settings.html')) {
-        let urlParams = new URLSearchParams(window.location.search);
-        let parameterValue = urlParams.get('uid')
-        let preferencesArr = [];
+        gameListElements.forEach((listElement, index) => {
+          listElement.addEventListener('click', () => {
+            gameListElements.forEach(listElement => {
+              listElement.classList.remove('selected');
+            });
+            listElement.classList.add('selected');
+            selectedGameUID = gameArr[index].uid;
+          });
+        });
+      })
+      .catch(err => {
+        console.error('Fetch error:', err);
+      });
+  } else if (pathname.includes('/ssa-project/website/pages/game-settings.html')) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const parameterValue = urlParams.get('uid');
+    let preferencesArr = [];
 
-        fetch(`https://cakelab.co.nl/ssa-server/?uid=${parameterValue}`, {method: 'GET'})
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const preferenceList = document.getElementById('preference-list');
-                preferencesArr = data.preferences;
-                preferencesArr.forEach(preference => {
-                    const preferenceElement = createPreferenceElement(preference);
-                    preferenceList.append(preferenceElement);
-                })
-            })
-            .catch(err => {
-                console.error('Fetch error:', err);
-            })
-    }
+    fetch(`https://cakelab.co.nl/ssa-server/?uid=${parameterValue}`, { method: 'GET' })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const preferenceList = document.getElementById('preference-list');
+        preferencesArr = data.preferences;
+        preferencesArr.forEach(preference => {
+          const preferenceElement = createPreferenceElement(preference);
+          preferenceList.append(preferenceElement);
+        });
+      })
+      .catch(err => {
+        console.error('Fetch error:', err);
+      });
+  }
 
-    let nextButton = document.getElementById('next-button');
-    let backButton = document.getElementById('back-button');
+  const nextButton = document.getElementById('next-button');
+  const backButton = document.getElementById('back-button');
 
-    if (backButton != null && nextButton != null) {
-        backButton.addEventListener('click', () => {
-            window.history.go(-1);
-        })
+  if (backButton != null && nextButton != null) {
+    backButton.addEventListener('click', () => {
+      window.history.go(-1);
+    });
 
-        nextButton.addEventListener('click', () => {
-            let pathname = window.location.pathname;
-            if (pathname.includes('/ssa-project/website/pages/game-selection.html')){
-                if (document.querySelectorAll('.selected').length > 0) {
-                    window.location.href = './game-settings.html?uid=' + encodeURIComponent(selectedGameUID);
-                } else {
-                    alert('Please select a game!');
-                }
-            } else if (pathname.includes('/ssa-project/website/pages/game-settings.html')) {
-                window.location.href = './game-status.html';
-            }
-        })
-    }
-})
+    nextButton.addEventListener('click', () => {
+      const pathname = window.location.pathname;
+      if (pathname.includes('/ssa-project/website/pages/game-selection.html')) {
+        if (document.querySelectorAll('.selected').length > 0) {
+          window.location.href = './game-settings.html?uid=' + encodeURIComponent(selectedGameUID);
+        } else {
+          alert('Please select a game!');
+        }
+      } else if (pathname.includes('/ssa-project/website/pages/game-settings.html')) {
+        window.location.href = './game-status.html';
+      }
+    });
+  }
+});
