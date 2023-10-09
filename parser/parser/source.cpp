7603f939556a5771c726bb24c6417bced89c01fe
Octron07 <feli.haselberger@gmail.com>
2023-10-09 10:31:03 +0200

    [SSAP-20] changed .gitignore file and renamed variables according to C++ conventions

--- parser/parser/source.cpp
+++ parser/parser/source.cpp
@@ -4,45 +4,45 @@
 
 int main()
 {
-    std::string* inputstring = readfile();
+    std::string* inputString = readFile();
 }
 
-std::string* readfile() {
+std::string* readFile() {
     // Read filename
     std::cout << "Please input a filename: ";
-    std::string filename;
-    std::cin >> filename;
-    std::ifstream inputfile(filename); // create stream
+    std::string fileName;
+    std::cin >> fileName;
+    std::ifstream inputFile(fileName); // create stream
 
     try {
-        if (!inputfile.is_open()) {
+        if (!inputFile.is_open()) {
             throw 1; // File not found
         }
         else {
             // read first line and check for data
-            std::string firstline;
-            getline(inputfile, firstline);
+            std::string firstLine;
+            getline(inputFile, firstLine);
 
             // First three bytes should be 72, 65 and 74 (HAJ) when decoded to uints
-            unsigned int filesignature[3] = { 
-                std::stoi(firstline.substr(0, 2), 0, 16), 
-                std::stoi(firstline.substr(2, 2), 0, 16), 
-                std::stoi(firstline.substr(4, 2), 0, 16) 
+            unsigned int fileSignature[3] = { 
+                std::stoi(firstLine.substr(0, 2), 0, 16), 
+                std::stoi(firstLine.substr(2, 2), 0, 16), 
+                std::stoi(firstLine.substr(4, 2), 0, 16) 
             };
 
-            if (filesignature[0] == 72 && filesignature[1] == 65 && filesignature[2] == 74) {
+            if (fileSignature[0] == 72 && fileSignature[1] == 65 && fileSignature[2] == 74) {
                 // read data from file and store it into array
-                int nroflines = 0;
+                int nrOfLines = 0;
 
-                std::string* inputarray = new std::string[nroflines];
+                std::string* inputArray = new std::string[nrOfLines];
 
                 int arrayindex = 0;
-                while (!inputfile.eof()) {
-                    getline(inputfile, inputarray[nroflines]);
+                while (!inputFile.eof()) {
+                    getline(inputFile, inputArray[nrOfLines]);
                 }
 
-                inputfile.close(); // clean up
-                return inputarray;
+                inputFile.close(); // clean up
+                return inputArray;
             } 
             else {
                 throw 3;
@@ -53,16 +53,16 @@ std::string* readfile() {
     catch (int err) {
         switch (err) {
         case 1: // File not found
-            std::cout << "[ERROR]: No file \"" + filename + "\" found." << std::endl;
-            inputfile.close();
+            std::cout << "[ERROR]: No file \"" + fileName + "\" found." << std::endl;
+            inputFile.close();
             break;
         case 2: // File too long
-            std::cout << "[ERROR]: The file \"" + filename + "\" is longer than stated in the first parameter of the file.";
-            inputfile.close();
+            std::cout << "[ERROR]: The file \"" + fileName + "\" is longer than stated in the first parameter of the file.";
+            inputFile.close();
             break;
         case 3: // Invalid File signature
-            std::cout << "[ERROR]: The file \"" + filename + "\" does not have the correct signature.";
-            inputfile.close();
+            std::cout << "[ERROR]: The file \"" + fileName + "\" does not have the correct signature.";
+            inputFile.close();
             break;
         }
     }
