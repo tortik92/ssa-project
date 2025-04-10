517859da02f0f5e4a711eecf2899067e96d00393
Artem <70284002+tortik92@users.noreply.github.com>
2025-04-10 09:35:47 +0200

    Update ViewController.swift

--- ios-app/SoundLeap/SoundLeap/ViewController.swift
+++ ios-app/SoundLeap/SoundLeap/ViewController.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class MyPeripheral: NSObject {
+
+        /// MARK: - BLE services and charcteristics Identifiers
+
+        public static let mainUUID     = CBUUID.init(string: "0000ffe0-0000-1000-8000-00805f9b34fb")
+        public static let secondary   = CBUUID.init(string: "0000ffe1-0000-1000-8000-00805f9b34fb")
+}
+
+// Extension to handle UITextFieldDelegate methods
+extension ViewController: UITextFieldDelegate, UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 
     @IBOutlet weak var digit6field: UITextField! // Text field for 6-digit input
     
@@ -17,10 +27,85 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-}
 
-// Extension to handle UITextFieldDelegate methods
-extension ViewController: UITextFieldDelegate {
+    let deviceUUID: [UInt8] = Array((UIDevice.current.identifierForVendor?.uuidString)!.utf8 ?? "ERR ".utf8)
+    
+    var centralManager: CBCentralManager!
+    var myPeripheral: CBPeripheral!
+    
+    private var values: CBCharacteristic?
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            print("BLE powered on")
+            // Turned on
+            central.scanForPeripherals(withServices: [MyPeripheral.mainUUID], options: nil)
+        }
+        else {
+            print("Something wrong with BLE")
+            // Not on, but can have different issues
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if let pname = peripheral.name {
+            if pname == "SL-" + textField.text{
+                print("Connected to: " + pname)
+                self.centralManager.stopScan()
+                
+                self.myPeripheral = peripheral
+                self.myPeripheral.delegate = self
+                self.centralManager.connect(peripheral, options: nil)
+            }
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected to " + peripheral.name!)
+        self.myPeripheral.discoverServices([MyPeripheral.mainUUID])
+    }
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("Disconnected from " + peripheral.name!)
+        central.scanForPeripherals(withServices: [MyPeripheral.mainUUID], options: nil)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                print(service.uuid.uuidString, service.description)
+                peripheral.discoverCharacteristics([MyPeripheral.mainUUID, MyPeripheral.secondary], for: service)
+            }
+        }
+    }
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?)
+    {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if(characteristic.uuid == MyPeripheral.secondary) {
+                    values = characteristic
+                }
+            }
+        }
+    }
+    
+    private func writeData( characteristic: CBCharacteristic, value: Data, withNewLine: Bool) {
+        let valuesToSend = Data(value)
+        let separator: UInt8 = 0x7E
+        var output: Data = Data(deviceUUID)
+        output.append(separator)
+        output.append(valuesToSend)
+        
+        if(withNewLine) {
+            let newLine: [UInt8] = [0x0A,0x0D]
+            output.append(Data(newLine))
+        }
+        
+        // Check if it has the write property
+        if characteristic.properties.contains(.writeWithoutResponse) && myPeripheral != nil {
+            myPeripheral.writeValue(output, for: characteristic, type: .withoutResponse)
+            
+        }
+
+    }
 
     // This method is called every time the user types or deletes a character in the text field
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -39,6 +124,7 @@ extension ViewController: UITextFieldDelegate {
         // Enable the connect button if exactly 6 characters are entered
         if(updatedText.count == 6) {
             connectButton.isEnabled = true;
+            centralManager = CBCentralManager(delegate: self, queue: nil)
         } else {
             // Disable the button if it's not 6 characters
             connectButton.isEnabled = false;
