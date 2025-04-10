5890bf8c7b0fa8dca3cf4944350f59f6bc559a61
Artem <70284002+tortik92@users.noreply.github.com>
2025-04-10 08:52:50 +0200

    Update gamecode.php

--- server/gamecode.php
+++ server/gamecode.php
@@ -1,4 +1,10 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Max-Age: 1728000');
+
 // Get the UID from the URL
 $uid = basename($_GET["uid"]);
 
